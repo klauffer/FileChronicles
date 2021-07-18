90c37850792970df146b2e9acd6a5c1de4c0aaba
Kyle Lauffer <kcl5072@gmail.com>
2021-07-18 18:35:53 -0400

    Create a file without a transaction

--- FileChronicles.Tests/ChroniclerShould.cs
+++ FileChronicles.Tests/ChroniclerShould.cs
@@ -1,13 +1,27 @@
+using System.IO;
+using System.Text;
+using System.Threading;
 using Xunit;
 
 namespace FileChronicles.Tests
 {
     public class ChroniclerShould
     {
+
+        private string _testContents = "This is a test";
+
+        private byte[] GetTestContentsBytes() => Encoding.ASCII.GetBytes(_testContents);
+
         [Fact]
-        public void Test1()
+        public void CreateAFile()
         {
+            var path = "TestFile.txt";
             var chronicler = new Chronicler();
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+            CancellationToken token = tokenSource.Token;
+            chronicler.Create(path, GetTestContentsBytes(), token);
+            var text = File.ReadAllText(path);
+            Assert.Equal(_testContents, text);
         }
     }
 }
--- FileChronicles/Chronicler.cs
+++ FileChronicles/Chronicler.cs
@@ -1,6 +1,29 @@
-﻿namespace FileChronicles
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileChronicles
 {
+    /// <summary>
+    /// the object that keeps accounts of events through the history of a transaction
+    /// </summary>
     public sealed class Chronicler
     {
+
+        /// <summary>
+        /// Creates a file at the specified location with the contents being the specified Bytes
+        /// </summary>
+        /// <param name="path">The location where the file will be made</param>
+        /// <param name="bytes">the contents of the file</param>
+        /// <param name="cancellationToken">to cancel writing the file</param>
+        /// <returns></returns>
+        public Task Create(string path, byte[] bytes, CancellationToken cancellationToken = default)
+        {
+            if(!File.Exists(path))
+            {
+                return File.WriteAllBytesAsync(path, bytes, cancellationToken);
+            }
+            return Task.CompletedTask; 
+        }
     }
 }
