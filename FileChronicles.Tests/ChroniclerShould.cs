66aed0ac890c0ad5f6206e9b49e2ed46889bdc6d
Kyle Lauffer <kcl5072@gmail.com>
2021-07-18 17:24:19 -0400

    Updated gitignore and added test project

---
+++ FileChronicles.Tests/ChroniclerShould.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace FileChronicles.Tests
+{
+    public class ChroniclerShould
+    {
+        [Fact]
+        public void Test1()
+        {
+            var chronicler = new Chronicler();
+        }
+    }
+}
---
+++ FileChronicles/Chronicler.cs
@@ -0,0 +1,6 @@
+﻿namespace FileChronicles
+{
+    public sealed class Chronicler
+    {
+    }
+}
