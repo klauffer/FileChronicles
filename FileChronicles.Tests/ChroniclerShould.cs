3ed6ebead4c50b5c9fb01e85c52afe5a607c247a
Kyle Lauffer <kcl5072@gmail.com>
2021-07-20 20:01:27 -0400

    added Fail early test

--- FileChronicles.Tests/ChroniclerShould.cs
+++ FileChronicles.Tests/ChroniclerShould.cs
@@ -27,5 +27,27 @@ namespace FileChronicles.Tests
             Assert.True(File.Exists(fileName1));
             Assert.True(File.Exists(fileName2));
         }
+
+        [Fact]
+        public async Task StopOnFirstError()
+        {
+            var fileName1 = "TestFile1.txt";
+            var fileName1Duplicate = "TestFile1.txt";
+            var fileName2 = "TestFile2.txt";
+            using SafeFile safeFile1 = SafeFile.Clear(fileName1),
+                           safeFile1Duplicate = SafeFile.Create(fileName1Duplicate),//create the first file to cause error
+                           safeFile2 = SafeFile.Clear(fileName2);
+
+            var chronicler = Chronicler.Begin();
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+            CancellationToken token = tokenSource.Token;
+
+            chronicler.Create(fileName1, new byte[0], token);
+            chronicler.Create(fileName2, new byte[0], token);
+            var eventResult = await chronicler.Commit();
+
+            Assert.Equal(ErrorCode.FileAlreadyExists.ToString() ,eventResult.Match(() => "", errorCode => errorCode.ToString()));
+            Assert.False(File.Exists(fileName2));//The second file, although valid, was never made
+        }
     }
 }
